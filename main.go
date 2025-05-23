ac726d06ac6d8acb7ae970e60ec8cbae453a2887
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-05-22 22:07:55 -0400

    feat: implement rough draft of route querying
    chore: load API_KEY inside main.go

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"codeberg.org/bci/mta_tracker_fullstack/internal/availableRoutes"
+	"codeberg.org/bci/mta_tracker_fullstack/internal/geturl"
+	"github.com/joho/godotenv"
+)
+
+var searchResultsHTML = template.Must(template.New("results").Parse(`
+{{range .}}
+<tr>
+    <td>{{.}}</td>
+</tr>
+{{end}}
+`))
+
+// For now, the agency we use is fixed.
+const routesForAgencyURL = "https://bustime.mta.info/api/where/routes-for-agency/MTA%20NYCT.json"
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	godotenv.Load(".env")
+
+	apiKey := os.Getenv("API_KEY")
+
+	if apiKey == "" {
+		http.Error(w, "Missing API key", http.StatusInternalServerError)
+	}
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	jsonBytes, callErr := geturl.Call(routesForAgencyURL, map[string]string{
+		"key": apiKey,
+	})
+
+	if callErr != nil {
+		http.Error(w, "Call to routes-for-agency URL failed", http.StatusInternalServerError)
+	}
+
+	var routesInfo availableRoutes.AvailableRoutes
+
+	if err := json.Unmarshal(jsonBytes, &routesInfo); err != nil {
+		http.Error(w, "Failed to unmarshal JSON into routes-info struct", http.StatusInternalServerError)
+	}
+
+	if code := routesInfo.Code; code != 200 {
+		http.Error(w, "Non-200 code", http.StatusInternalServerError)
+	}
+
+	var results []string
+	routeQuery := strings.ToLower(r.FormValue("search"))
+
+	for _, route := range routesInfo.Data.List {
+		id := strings.ToLower(route.ID)
+
+		if strings.Contains(id, routeQuery) {
+			results = append(results, id)
+		}
+	}
+
+	if err := searchResultsHTML.Execute(w, results); err != nil {
+		http.Error(w, "Unable to render template", http.StatusInternalServerError)
+	}
+}
+
+func loadPage(w http.ResponseWriter, r *http.Request) {
+	indexHTML, err := os.Open("templates/index.html")
+
+	if err != nil {
+		http.Error(w, "Unable to read index.html template", http.StatusInternalServerError)
+	}
+
+	defer indexHTML.Close()
+
+	htmlBytes, err := io.ReadAll(indexHTML)
+
+	if err != nil {
+		http.Error(w, "Unable to convert index.html to bytes", http.StatusInternalServerError)
+	}
+
+	w.Write(htmlBytes)
+}
+
+func main() {
+	http.HandleFunc("/search", searchHandler)
+	http.HandleFunc("/", loadPage)
+	http.ListenAndServe(":8080", nil)
+}
